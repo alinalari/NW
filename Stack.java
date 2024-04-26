4088c377315774347e718d0e9239758860b8aec8
alinalari <65485525+alinalari@users.noreply.github.com>
2024-04-25 20:54:11 -0700

    Update Stack.java
    Using the put in Queue, not the put in stack. When a card is won it goes to the bottom of the other player's deck.

--- Stack.java
+++ Stack.java
@@ -5,18 +5,18 @@ public class Stack<E> extends LinkedList<E> {
 	
 	public Stack() {}
 	
-	public void put(E o) {
-		addFirst(o);
-	}
+	// public void put(E o) {
+	// 	addFirst(o);
+	// }
 	
-	public E get() {
-		if (!this.isEmpty()) {
-			return removeFirst();
-		} else {
-			System.err.println("You can\'t do that!");
-			return null;
-		} 
-	}
+	// public E get() {
+	// 	if (!this.isEmpty()) {
+	// 		return removeFirst();
+	// 	} else {
+	// 		System.err.println("You can\'t do that!");
+	// 		return null;
+	// 	} 
+	// }
 	
 	public E peek() {
 		return getFirst();
