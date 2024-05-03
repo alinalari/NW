50ed2f0d22fbd9d2c6d71425ee6f9b368da258ac
alinalari <65485525+alinalari@users.noreply.github.com>
2024-05-02 20:25:23 -0700

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,23 +1,26 @@
 
 import java.util.ArrayList;
 
+
 public class Main {
-	
+
 	static Player player1 = new Player("Player 1");
 	static Player player2 = new Player("Player 2");
 
 	public static void main(String[] args){
 
-		//Shuffles deck
+		// Shuffles deck
 		Deck deck = new Deck(); 
 		deck.shuffle();
 
-		// Deal half of the deck (26 cards) to each player
-		deal(deck, player1); 
+		// Deals each player half the deck
+		deal(deck, player1);  
 		deal(deck, player2);
 
+		//Begins game
 		playNotchWar(player1, player2);
 
+		//Prints winner at the end of the game
 		Player winner = determineWinner(player1, player2);
 		System.out.println();
 		System.out.println("The winner is... " + winner.getName() + "!");
@@ -35,8 +38,8 @@ public class Main {
 	public static void playNotchWar(Player player1, Player player2) { 
 		while (!player1.isEmpty() && !player2.isEmpty()) {
 			playRound(player1, player2);
-			
 		}
+
 	}
 
 	public static void playRound(Player player1, Player player2) {
@@ -45,20 +48,27 @@ public class Main {
 
 		System.out.println(player1.getName() + " plays " + card1 + ", " + player2.getName() + " plays " + card2);
 
-		if (card1.getRank() == card2.getRank()) { // Cards' rank are the same -> War!
+		// Cards are the same = tie -> War!
+		if (card1.compareTo(card2) == 0) { 
 			war(player1, player2);
 		}
-		else if (Math.abs(card1.getRank() - card2.getRank()) == 1) { // A player has a card one below their opponent -> Notched!
+
+		// One player has a card 1 below their opponent -> Notched!
+		else if (Math.abs(card1.getRank() - card2.getRank()) == 1) { 
 			System.out.println("Notched!");
 			if (card1.getRank() < card2.getRank()) {
 				player1.addCards(player1.get(), player2.get());
+
 			} else {
 				player2.addCards(player1.get(), player2.get());
+
 			}
-		} else if (card1.getRank() > card2.getRank()) {  // Like regular war -> Player with higher card wins their opponent's cards
+		} else if (card1.getRank() > card2.getRank()) {
 			player1.addCards(player1.get(), player2.get());
+
 		} else {
 			player2.addCards(player1.get(), player2.get());
+
 		}
 
 		System.out.println(player1.getName() + " has " + player1.size() + " cards, " +
@@ -66,68 +76,109 @@ public class Main {
 	}
 
 
-	public static void war(Player player1, Player player2) { // When there is a tie it is a war
+	public static void war(Player player1, Player player2) {
 		System.out.println("WAR!");
 		ArrayList<Card> cardsInPlay = new ArrayList<>();
 		ArrayList<Card> player1Last = new ArrayList<>();
 		ArrayList<Card> player2Last = new ArrayList<>();
 
+		boolean isTie;
+
 		do {
-			for (int i = 0; i < Math.min(5,player1.size()); i++) {
-				Card card = player1.get();
-				cardsInPlay.add(card);
-				player1Last.add(card);
+
+			isTie = false;
+
+			// Both players play 3 cards face down and 1 face up
+			if (player1.size() >= 4) {  
+				for (int i = 0; i < 4; i++) {
+					Card card = player1.get();
+					cardsInPlay.add(card);
+					player1Last.add(card);
+				}
+
+			} else {
+				for (int i = 0; i < player1.size(); i++) {
+					Card card = player1.get();
+					cardsInPlay.add(card);
+					player1Last.add(card);
+				}
 			}
 
-			
-			for (int i = 0; i < Math.min(5, player2.size()); i++) {
-				Card card = player2.get();
-				cardsInPlay.add(card);
-				player2Last.add(card);
+			if (player2.size() >= 4) {  
+				for (int i = 0; i < 4; i++) {
+					Card card = player2.get();
+					cardsInPlay.add(card);
+					player2Last.add(card);
+				}
+			} else {
+				for (int i = 0; i < player2.size(); i++) {
+					Card card = player2.get();
+					cardsInPlay.add(card);
+					player2Last.add(card);
+				}
 			}
 
+			// If there is a tie and then another tie
 			if (!player1.isEmpty() && !player2.isEmpty() &&
-					player1.peek().compareTo(player2.peek()) == 0) {  // 4th card placed face up is the same as the opponent -> Another tie!!
+					player1.peek().compareTo(player2.peek()) == 0) {
+				isTie = true;
 				System.out.println(player1.getName() + " plays " + player1.peek() + ", " +
 						player2.getName() + " plays " + player2.peek());
-				System.out.println("Another tie! WAR!");	
+				System.out.println("Another tie! WAR!");
 			} else {
+				if (isTie) {
+					isTie = false;
+				}
 				break; // Exit the loop if no tie
 			}
-		} while (cardsInPlay.size() < 52 && player1.size() >= 4 && player2.size() >= 4);
+		} while (isTie && cardsInPlay.size() < 52 && player1.size() >= 4 && player2.size() >= 4);
 
 		// Distribute the cards in play to the winner
 		Card player1Card = player1.isEmpty() ? null : player1.peek();
 		Card player2Card = player2.isEmpty() ? null : player2.peek();
+		if (player1Card != null) {
+			cardsInPlay.add(player1.get());
+		}
+		if (player2Card != null) {
+			cardsInPlay.add(player2.get());
+		}
 
 		if (player1Card != null && player2Card != null) {
+
 			if (player1Card.compareTo(player2Card) > 0) {
 				System.out.println(player1.getName() + " plays " + player1Card + ", " +
 						player2.getName() + " plays " + player2Card);
 				player1.addAll(cardsInPlay);
+
 			} else {
 				System.out.println(player1.getName() + " plays " + player1Card + ", " +
 						player2.getName() + " plays " + player2Card);
 				player2.addAll(cardsInPlay);
 			}
-		} else if (player1Card != null) {
-			player1.addAll(cardsInPlay);
-			System.out.println(player1.getName() + " plays " + player1Card + ", " +
-					player2.getName() + " plays " + player2Last.get(player2Last.size() - 1));
 
+		} else if (player1Card != null) {
+			Card card2 = player2Last.get(player2Last.size() - 1);
+			System.out.println(player1.getName() + " plays " + player1Card + ", " +
+					player2.getName() + " plays " + card2);
+			player1.addAll(cardsInPlay);
 
 		} else {
-			player2.addAll(cardsInPlay);
-			System.out.println(player1.getName() + " plays " + player1Last.get(player1Last.size() - 1) + ", " +
+			Card card1 = player1Last.get(player1Last.size() - 1);
+			System.out.println(player1.getName() + " plays " + card1 + ", " +
 					player2.getName() + " plays " + player2Card);
+			player2.addAll(cardsInPlay);
 		}
 	}
 
-	public static Player determineWinner(Player player1, Player player2) { // Whichever player has 0 cards first loses
+
+	// Whichever player has 0 cards first loses
+	public static Player determineWinner(Player player1, Player player2) { 
 		if (player1.isEmpty()) {
 			return player2;
 		} else {
 			return player1;
 		}
 	}
+
 }
+
